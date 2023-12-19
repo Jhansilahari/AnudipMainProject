cc23fe7f4e073a270f1e2d9820c2d2f53fff4b48
Jhansi Lahari Gandala <118799953+Jhansilahari@users.noreply.github.com>
2023-12-19 16:04:30 +0530

    Create UserRepository .java

---
+++ UserRepository .java
@@ -0,0 +1,16 @@
+package com.anudip.TrainTicketReservationSystemProject.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.anudip.TrainTicketReservationSystemProject.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>  {
+
+	Optional<User> findByEmail(String email);
+
+
+
+}
